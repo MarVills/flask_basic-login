17fe2c2e5778a140d1c5c0b7cfd6f2a3ae9c74f4
MarVills <villaflormarbenc@gmail.com>
2022-07-10 22:48:30 +0800

    push project

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import (Flask, render_template)
+
+class User:
+  def __init__(self, id, username, password):
+    self.id = id
+    self.username = username
+    self.password = password
+  
+  def __repr__(self):
+    return f'<User: {self.username}>'
+
+users = []
+users.append(User(id=1, username='Anthony', password='passwrod'))
+
+print(users)
+
+
+# app = Flask(__name__)
+
+# @app.route('/login')
+# def login():
+#   return render_template("login.html")
+
+# @app.route('/profile')
+# def profile():
+#   return render_template("profile.html")
