4b8dc6533066b98ee684c48fc34774846b3d382b
MarVills <villaflormarbenc@gmail.com>
2022-07-11 16:39:12 +0800

    finished basic functions, finalization on process

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from flask import (Flask, render_template)
+from crypt import methods
+from flask import (
+  Flask, 
+  render_template, 
+  request, 
+  session, 
+  redirect, 
+  url_for,
+  g,
+  )
 
 class User:
   def __init__(self, id, username, password):
@@ -10,17 +19,41 @@ class User:
     return f'<User: {self.username}>'
 
 users = []
-users.append(User(id=1, username='Anthony', password='passwrod'))
+users.append(User(id=1, username='Anthony', password='password'))
+users.append(User(id=2, username='Becca', password='secret'))
+users.append(User(id=3, username='Carlos', password='somethingsimple'))
 
-print(users)
 
+app = Flask(__name__)
+app.secret_key = 'somesecretkeythatonlyishouldknow'
 
-# app = Flask(__name__)
+@app.before_request
+def before_request():
+  g.user = None
+  if 'user_id' in session:
+    user = [x for x in users if x.id == session['user_id']][0]
+    g.user = user
 
-# @app.route('/login')
-# def login():
-#   return render_template("login.html")
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    session.pop('user_id', None)
 
-# @app.route('/profile')
-# def profile():
-#   return render_template("profile.html")
+    username = request.form['username']
+    password = request.form['password']
+
+    user = [x for x in users if x.username == username][0]
+
+    if user and user.password == password:
+      session['user_id'] = user.id
+      return redirect(url_for('profile'))
+    
+    return redirect(url_for('login'))
+
+  return render_template("login.html")
+
+@app.route('/profile')
+def profile():
+  if not g.user:
+    return redirect(url_for('login'))
+  return render_template("profile.html")
